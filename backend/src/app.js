b933cffb4bcb7d8cd553acbf337a9f7413ab4080
Jondalff <oliverk.kostamo@gmail.com>
2020-02-26 16:12:11 +0200

    add more func

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const logger = require('koa-logger');
 const cors = require('kcors');
+const {OP} = require('sequelize')
 
 const database = require('./database');
 
@@ -19,9 +20,10 @@ app.use(bodyParser());
 /* METHODS TO RESPOND TO THE ROUTES */
 
 const listChats = async (ctx) => {
-  const options = {};
+  const {room} = ctx.query;
+  const options = room ? { room: {[Op.eq]: room}} : {};
 
-  const chats = await database.Chat.findAll(options);
+  const chats = await database.Chat.findAll({where: options});
 
   const response = {
     results: chats,
@@ -31,9 +33,10 @@ const listChats = async (ctx) => {
 };
 
 const createChat = async (ctx) => {
+  
   const params = ctx.request.body;
 
-  const chat = await database.Chat.create({ message: params.message  });
+  const chat = await database.Chat.create({ message: params.message, nick: params.nick, room: params.room,});
 
   ctx.body = chat;
   ctx.status = 201;
